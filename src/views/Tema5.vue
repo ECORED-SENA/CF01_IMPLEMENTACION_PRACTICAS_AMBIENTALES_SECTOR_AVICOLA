053a7e9084c94c5f53dabde61e04b042493545e1
Cielo-Angulo <cielodamarisangulo@gmail.com>
2025-10-16 06:29:53 -0500

    actualización YT

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -11,7 +11,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/UpdMxPZeTIk?si=huQ6A2g53gt1AS5z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Video: Leyenda del video
 
     .bg-color-9.tarjeta.mb-5
